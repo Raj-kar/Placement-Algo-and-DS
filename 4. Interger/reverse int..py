eeda7eb7d815e526b3bc254714c7f7d8897110db
FlutterRaj <rajkar921@gmail.com>
2020-10-18 17:01:46 +0530

    add validate on reverse int

--- 4. Interger/reverse int..py
+++ 4. Interger/reverse int..py
@@ -1,20 +1,28 @@
 def reverse_int(num):
     negative = False
-    if (num < 0):
-        negative = True
-        num = num * - 1  # also can use abs(num)
+    try:
+        num = int(num)
+        if (num < 0):
+            negative = True
+            num = num * - 1  # also can use abs(num)
 
-    reverse = 0
-    while(num):
-        reverse = (reverse * 10) + (num % 10)
-        num = round(num / 10)
+        reverse = 0
+        while(num):
+            reverse = (reverse * 10) + (num % 10)
+            num = round(num / 10)
 
-    if negative:
-        return reverse * -1
-    else:
-        return reverse
+        if negative:
+            return reverse * -1
+        else:
+            return reverse
+    except:
+        return "Invalid Input"
 
 
 print(reverse_int(321))
 print(reverse_int(-123))
 print(reverse_int(210))
+
+# validation check
+print(reverse_int("200"))   # 2
+print(reverse_int("hey reversed !"))
